5300e8a9ef59f1a6be2dda6cf6b0fce0ab3513f5
Christophe Sinclair <christophe.sinclair@orange.fr>
2022-11-06 17:34:10 +0100

    Add files via upload

---
+++ 150-Alimentation_Programmable/clientWeb/js/libFunctions.js
@@ -0,0 +1,10 @@
+// *******************************
+// Images de fond
+// *******************************
+$(function() {
+	$("body").css('background-image', "url('images/font.jpg')");
+	$("body").css('background-attachment', 'local');
+	$("body").css('background-size', 'fixed');
+	$("body").css('background-repeat', 'no-repeat');
+	$("body").css('background-position','left top');
+});
